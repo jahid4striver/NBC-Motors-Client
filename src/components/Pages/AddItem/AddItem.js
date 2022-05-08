5c18e3840fe2802182d16035dec3565632bad4c6
jahid4striver <jahidhassn701@gmail.comr>
2022-05-08 12:35:52 +0600

    Delete Item Created

---
+++ src/components/Pages/AddItem/AddItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddItem = () => {
+    return (
+        <div>
+            <h1>Add a New Item</h1>
+        </div>
+    );
+};
+
+export default AddItem;
--- src/components/Pages/ManageInventories/ManageInventories.js
+++ src/components/Pages/ManageInventories/ManageInventories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import ManageInventory from '../ManageInventory/ManageInventory';
 
 const ManageInventories = () => {
@@ -12,7 +13,10 @@ const ManageInventories = () => {
     },[])
     return (
         <div>
-            <h1>Total Bikes: {bikes.length}</h1>
+            <h1 className='text-2xl my-5 text-red-600'>Total Bikes: {bikes.length}</h1>
+            <div className='container my-4 lg:text-right md:text-right'>
+            <Link to='/additem' className="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add New Item</Link>
+            </div>
             <div className='container grid lg:grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1'>
             {
                 bikes.map(bike=><ManageInventory
